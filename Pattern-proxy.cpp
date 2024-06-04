5aa0fc7aed21492723a67bdee477e0fe6732e225
SkeTch <sketch13rus@mail.ru>
2024-06-04 14:45:41 +0300

    Initial commit

---
+++ Pattern-proxy.cpp
@@ -0,0 +1,37 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+
+class VeryHeavyDatabase {
+public:
+    std::string GetData(const std::string& key) const noexcept {
+        return "Very Big Data String: " + key;
+    }
+};
+
+class OneShotDB : public VeryHeavyDatabase {
+private:
+    VeryHeavyDatabase* real_object_;
+    mutable size_t shots_left_;
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots=1) : real_object_(real_object), shots_left_(shots) {}
+    std::string GetData(const std::string& key) const noexcept {
+        if (shots_left_ > 0) {
+            --shots_left_;
+            return real_object_->GetData(key);
+        }
+        else {
+            return "error";
+        }
+    }
+};
+
+int main() 
+{
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(std::addressof(real_db), 2);
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    return 0;
+}
